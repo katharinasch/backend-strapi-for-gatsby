27737908b913cd42b39d76e89bebcb5dc3da6824
Katharina Schreiber <ms.schreiber.katharina@gmail.com>
2021-04-26 16:56:12 +0200

    first commit

---
+++ config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '3106178f5f57b95f928b0effd59361f1'),
+    },
+  },
+});
---
+++ extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'e3fd5278-f89e-45e5-9fd7-e428b702b6ce'
+};
